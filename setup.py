79eb3dc8b010f2715eaa5c5823d9a47b7bdbbc0a
Stefano Fontanelli <s.fontanelli@asidev.com>
2012-08-03 15:59:41 +0200

    Fix setup.py information.

--- setup.py
+++ setup.py
@@ -1,26 +1,30 @@
-from setuptools import setup, find_packages
-import sys, os
+from setuptools import find_packages
+from setuptools import setup
 
-version = '0.1'
+version = '0.1a1'
 
 setup(name='CrudAlchemy',
       version=version,
-      description="",
+      description="Helper which encapsulates CRUD operations on SQLAlchemy models.",
       long_description="""\
-""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='',
+An helper class which encapsulates common CRUD operations on SQLAlchemy mapped classes.""",
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Intended Audience :: Developers',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 2.7',
+                   'License :: OSI Approved :: MIT License',
+                   'Operating System :: OS Independent',
+                   'Topic :: Database'],
+      keywords='crud sqlalchemy',
       author='Stefano Fontanelli',
       author_email='s.fontanelli@asidev.com',
-      url='',
+      url='https://github.com/stefanofontanelli/CrudAlchemy',
       license='MIT',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      packages=find_packages(),
       include_package_data=True,
-      zip_safe=False,
+      zip_safe=True,
       install_requires=[
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
+            'SQLAlchemy'],
+      tests_require=[],
+      test_suite='tests',
       )
